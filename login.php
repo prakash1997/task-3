5c1fd08034dff18d209eae6e598e2e86a4f7eebc
prakash1997 <prakashkichu97@gmail.com>
2015-07-17 02:01:56 +0530

    login

---
+++ login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+</head>
+<body>
+<style>
+body{ background-color:red;}
+</style>
+<div class="row">
+<div class="col-sm-6" id="quiz" style="background-color:blue;height:200px;font-size:180%;margin-top:75px;padding:50px 50px;">
+<center>
+<a href="question.php">QUIZ QUESTIONS</a></div>
+<div class="col-sm-6" id="quizzz" style="background-color:green;height:200px;font-size:180%;margin-top:75px;padding:50px 50px;">
+<center><a href="ans.php">ANSWER THE QUESTIONS BELOW</a></center>
+</div>
+</center>
+</body>
+
+</html>
